9cd3a6357f31f1c77b518f3e14fb0f9a32c3a593
cDillanb <85910223+cDillanb@users.noreply.github.com>
2021-09-14 09:21:55 -0600

    added rollbar

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 4000;
+const Rollbar = require("rollbar");
+
+let rollbar = new Rollbar({
+    accessToken: "ec5c2c5891774868946da8086e39a6c1",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
+    rollbar.info("HTML file served successfully!");
 });
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
